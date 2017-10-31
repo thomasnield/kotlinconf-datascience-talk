d9ee49bd0900b277b7c9ad19ae83e32934d7b146
thomasnield <thomasnield@live.com>
2017-10-31 13:34:47 -0500

    ex6

--- examples/src/main/kotlin/KotlinConfDemo.kt
+++ examples/src/main/kotlin/KotlinConfDemo.kt
@@ -1,32 +1,73 @@
-import org.nield.kotlinstatistics.binByComparable
+import javafx.geometry.Orientation
+import javafx.scene.chart.NumberAxis
+import org.nield.kotlinstatistics.multiKMeansCluster
+import tornadofx.*
 import java.time.LocalDate
-import java.time.Month
+import java.time.temporal.ChronoUnit
 
-fun main(args: Array<String>) {
+class MyApp: App(MyView::class)
 
-    data class Sale(val accountId: Int, val date: LocalDate, val amount: Double)
+class MyView : View() {
+    override val root = splitpane {
+        orientation = Orientation.HORIZONTAL
 
-    val sales = listOf(
-            Sale(1, LocalDate.of(2016,12,3), 180.0),
-            Sale(2, LocalDate.of(2016, 7, 4), 140.2),
-            Sale(3, LocalDate.of(2016, 6, 3), 111.4),
-            Sale(4, LocalDate.of(2016, 1, 5), 192.7),
-            Sale(5, LocalDate.of(2016, 5, 4), 137.9),
-            Sale(6, LocalDate.of(2016, 3, 6), 125.6),
-            Sale(7, LocalDate.of(2016, 12,4), 164.3),
-            Sale(8, LocalDate.of(2016, 7,11), 144.2)
-    )
+        tableview<Patient> {
+            column("FIRST NAME", Patient::firstName)
+            column("LAST NAME", Patient::lastName)
+            column("GENDER", Patient::gender)
+            column("BIRTHDAY", Patient::birthday)
+            column("AGE", Patient::age)
+            column("WBCC", Patient::whiteBloodCellCount)
 
-    //bin by quarter
-    val binnedByQuarter = sales.binByComparable(
-            valueSelector = { it.date.month },
-            binIncrements = 3,
-            incrementer = { it.plus(1) }
-    )
+            items.setAll(patients)
+        }
 
-    binnedByQuarter.forEach(::println)
+        scatterchart("WBCC Clustering by Age", NumberAxis(), NumberAxis()) {
 
-    //look up bin for February
-    println("\n\nLook up bin for FEBRUARY")
-    println(binnedByQuarter[Month.FEBRUARY])
+            patients.multiKMeansCluster(k = 3,
+                        maxIterations = 10000,
+                        trialCount = 50,
+                        xSelector = { it.age.toDouble() },
+                        ySelector = { it.whiteBloodCellCount.toDouble() }
+                    )
+                    .forEachIndexed { index, centroid ->
+                        series("Group ${index + 1}") {
+                            centroid.points.forEach {
+                                data(it.age, it.whiteBloodCellCount)
+                            }
+                        }
+                    }
+        }
+    }
+}
+
+
+data class Patient(val firstName: String,
+                   val lastName: String,
+                   val gender: Gender,
+                   val birthday: LocalDate,
+                   val whiteBloodCellCount: Int)  {
+
+    val age = ChronoUnit.YEARS.between(birthday, LocalDate.now())
+}
+
+val patients = listOf(
+        Patient("John", "Simone", Gender.MALE, LocalDate.of(1989, 1, 7), 4500),
+        Patient("Sarah", "Marley", Gender.FEMALE, LocalDate.of(1970, 2, 5), 6700),
+        Patient("Jessica", "Arnold", Gender.FEMALE, LocalDate.of(1980, 3, 9), 3400),
+        Patient("Sam", "Beasley", Gender.MALE, LocalDate.of(1981, 4, 17), 8800),
+        Patient("Dan", "Forney", Gender.MALE, LocalDate.of(1985, 9, 13), 5400),
+        Patient("Lauren", "Michaels", Gender.FEMALE, LocalDate.of(1975, 8, 21), 5000),
+        Patient("Michael", "Erlich", Gender.MALE, LocalDate.of(1985, 12, 17), 4100),
+        Patient("Jason", "Miles", Gender.MALE, LocalDate.of(1991, 11, 1), 3900),
+        Patient("Rebekah", "Earley", Gender.FEMALE, LocalDate.of(1985, 2, 18), 4600),
+        Patient("James", "Larson", Gender.MALE, LocalDate.of(1974, 4, 10), 5100),
+        Patient("Dan", "Ulrech", Gender.MALE, LocalDate.of(1991, 7, 11), 6000),
+        Patient("Heather", "Eisner", Gender.FEMALE, LocalDate.of(1994, 3, 6), 6000),
+        Patient("Jasper", "Martin", Gender.MALE, LocalDate.of(1971, 7, 1), 6000)
+)
+
+enum class Gender {
+    MALE,
+    FEMALE
 }
