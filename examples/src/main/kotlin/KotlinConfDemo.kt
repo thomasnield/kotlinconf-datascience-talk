9ad746dc9a561f543f934ab03a36788ac9370930
thomasnield <thomasnield@live.com>
2017-10-31 13:28:16 -0500

    ex4

--- examples/src/main/kotlin/KotlinConfDemo.kt
+++ examples/src/main/kotlin/KotlinConfDemo.kt
@@ -1,47 +1,32 @@
-import org.nield.kotlinstatistics.averageBy
-import org.nield.kotlinstatistics.countBy
-
-//declare Product class
-class Product(val id: Int,
-              val name: String,
-              val category: String,
-              val section: Int,
-              val defectRate: Double)
-
-// Create list of Products
-val products = listOf(Product(1, "Rayzeon", "A", 3, 1.1),
-        Product(2, "ZenFire", "B", 4, 0.7),
-        Product(3, "HydroFlux", "A", 3, 1.9),
-        Product(4, "IceFlyer", "C", 1, 2.4),
-        Product(5, "FireCoyote", "B", 4, 3.2),
-        Product(6, "LightFiber", "B",2,  5.1),
-        Product(7, "PyroKit", "A", 3, 1.4),
-        Product(8, "BladeKit", "C", 1, 0.5),
-        Product(9, "NightHawk", "C", 1, 3.5),
-        Product(10, "NoctoSquirrel", "A", 2, 1.1),
-        Product(11, "WolverinePack", "A", 3, 1.2)
-)
-
+import org.nield.kotlinstatistics.binByComparable
+import java.time.LocalDate
+import java.time.Month
 
 fun main(args: Array<String>) {
 
-    // Data Class for Grouping
-    data class Key(val category: String, val section: Int)
+    data class Sale(val accountId: Int, val date: LocalDate, val amount: Double)
 
-    // Get Count by Category and Section
-    val countByCategoryAndSection =
-            products.countBy { Key(it.category, it.section) }
+    val sales = listOf(
+            Sale(1, LocalDate.of(2016,12,3), 180.0),
+            Sale(2, LocalDate.of(2016, 7, 4), 140.2),
+            Sale(3, LocalDate.of(2016, 6, 3), 111.4),
+            Sale(4, LocalDate.of(2016, 1, 5), 192.7),
+            Sale(5, LocalDate.of(2016, 5, 4), 137.9),
+            Sale(6, LocalDate.of(2016, 3, 6), 125.6),
+            Sale(7, LocalDate.of(2016, 12,4), 164.3),
+            Sale(8, LocalDate.of(2016, 7,11), 144.2)
+    )
 
-    println("Counts by Category and Section")
-    countByCategoryAndSection.entries.forEach { println(it) }
+    //bin by quarter
+    val binnedByQuarter = sales.binByComparable(
+            valueSelector = { it.date.month },
+            binIncrements = 3,
+            incrementer = { it.plus(1) }
+    )
 
-    // Get Average Defect Rate by Category and Section
-    val averageDefectByCategoryAndSection =
-            products.averageBy(
-                    keySelector = { Key(it.category, it.section) },
-                    doubleSelector = { it.defectRate }
-            )
+    binnedByQuarter.forEach(::println)
 
-    println("\nAverage Defect Rate by Category and Section")
-    averageDefectByCategoryAndSection.entries.forEach { println(it) }
+    //look up bin for February
+    println("\n\nLook up bin for FEBRUARY")
+    println(binnedByQuarter[Month.FEBRUARY])
 }
