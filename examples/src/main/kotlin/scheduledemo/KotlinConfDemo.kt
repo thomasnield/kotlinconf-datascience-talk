428aac9ab77213d29dc855937bb4da6c3dca4044
Thomas Nield <thomasnield@live.com>
2017-11-01 07:36:50 -0500

    reorg

---
+++ examples/src/main/kotlin/scheduledemo/KotlinConfDemo.kt
@@ -0,0 +1,25 @@
+
+
+
+fun main(args: Array<String>) {
+
+    drivers.forEach { it.addToModel() }
+    model.minimise().let(::println)
+
+    // see variables for each driver
+    drivers.forEach {
+        println("Driver ${it.driverNumber}: ${it.shiftStart.value.toInt()}-${it.shiftEnd.value.toInt()}")
+    }
+}
+
+// parameters
+val operatingDay = 6..22
+val operatingDayLength = operatingDay.endInclusive - operatingDay.start
+val allowableShiftSize = 4..6
+
+// Map drivers by their ID for ad hoc retrieval
+val drivers = listOf(
+        Driver(driverNumber=1, rate =10.0),
+        Driver(driverNumber=2, rate =12.0, availability=6..11),
+        Driver(driverNumber=3, rate =14.0)
+)
